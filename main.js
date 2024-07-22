a4ea19a3f16e5cd42e9c1f8967c04983fe6a1e14
Aleksei Chesnokov <173167461+Aleksei-Che@users.noreply.github.com>
2024-07-22 14:12:13 +0200

    Exercici 1.3: Callback

--- main.js
+++ main.js
@@ -137,3 +137,85 @@ const postiuNegatiuZero = (num) => {
     processar(23, mostrarNum);
 
 
+//    Exercici 2
+// Callbacks amb operacions matemàtiques: Escriu una funció calculadora que accepti tres paràmetres: dos nombres i una funció de callback. La funció calculadora ha d'invocar la funció de callback amb els dos nombres com a paràmetres. Després, crida calculadora amb una funció que faci la suma dels dos nombres.
+
+const calculadora = (num1, num2, callback) => {
+
+    return callback(num1, num2);
+    
+    }
+
+    const suma = (a, b) => {
+        return a + b;
+    }
+
+    let total = calculadora(4, 9, suma);
+
+    console.log("El resultat de la suma és: ", total);
+
+
+
+// Nivell 2
+
+
+// Exercici 3
+// Ús de callbacks en funcions asíncrones: Escriu una funció esperarISaludar que accepti dos paràmetres: un nom i una funció de callback. La funció ha d'esperar 2 segons i llavors invocar la funció de callback, passant el nom com a paràmetre.
+
+
+async function esperarISaludar() {
+
+    let promise = new Promise ((resolve, reject) => {
+
+        setTimeout(() => resolve ("Jaume"), 2000 );
+    });
+
+    let result = await promise;
+
+    console.log(result);
+}       
+ 
+esperarISaludar();
+
+
+
+// Exercici 4
+// Callbacks amb arrays: Escriu una funció processarElements que accepti dos paràmetres: un array i una funció de callback. La funció processarElements ha d'invocar la funció de callback per cada element de l'array.
+
+const processarElements = (arr3, fn) => {
+
+    arr3.forEach(item => fn(item));
+
+}
+
+const multiplicarPerTres = (num) => {
+    console.log(num * 3);
+}
+
+let arr3 = [2, 3, 4, 5];
+
+processarElements(arr3, multiplicarPerTres)
+
+
+// Nivell 3
+// Exercici 5
+
+// Escriu una funció processarCadena que accepti dos paràmetres: una cadena de caràcters i una funció de callback. La funció processarCadena ha de convertir la cadena a majúscules i llavors invocar la funció de callback amb la cadena transformada.
+
+const processarCadena =(cadena, fncallback)=>{
+
+   let cadenaMajusc = cadena.toUpperCase();
+
+   fncallback(cadenaMajusc);
+}
+
+let mostrarCadena = (cadena) => {
+
+    console.log(cadena);
+}
+
+let cadena = "bon dia, com va?";
+
+processarCadena(cadena, mostrarCadena);
+
+
