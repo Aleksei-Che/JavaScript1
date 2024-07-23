20b2d6f67dff73634a6a41ba8db655cfd953cb7c
Aleksei Chesnokov <173167461+Aleksei-Che@users.noreply.github.com>
2024-07-23 11:27:25 +0200

    Exercici 1.7: Promises & Async/Await

--- main.js
+++ main.js
@@ -435,3 +435,106 @@ for (let [index, nom] of nomsPerIndex.entries()) {
 
     console.log(`Index: ${index}, Nom: ${nom}`);
 };
+
+// Exercici 1.7: Promises & Async/Await
+
+// Nivell 1
+// Exercici 1
+// Creació d'una Promesa: Crea una promesa que es resolgui després de 2 segons i que retorni la cadena de text 'Hola, món'.
+
+// Exercici 2
+// Utilització d'una Promesa: Utilitza la promesa creada en l'exercici anterior. Crea un .then que imprimeixi el resultat a la consola.
+
+const promesa = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        resolve("Hola, mon");
+    }, 2000);
+})
+
+promesa.then(result => {
+    console.log(result);
+});
+
+
+
+
+
+// Exercici 3
+// Promesa amb reject: Crea una promesa que es resolgui després de 2 segons si l'input és igual a 'Hola', i que la rebutgi si l'input és qualsevol altra cosa.
+
+const promessa1 = (input) => {
+    return new Promise((resolve, reject) => {
+        setTimeout (() =>{
+            if (input === "Hola"){
+                resolve ("Hola");
+            } else {
+
+                reject ("Error");
+            }
+        });
+    })
+}
+
+promessa1("Hola").then(result => {
+    console.log(result);
+}).catch(error => {
+    console.log(error);
+})
+
+// Exercici 4
+// Ús de async/await: Escriu una funció asíncrona que utilitzi la funció await per a esperar el resultat de la promesa creada a l'exercici 1, i que després imprimeixi aquest resultat a la consola.
+// ++ Nivell 2
+
+// Exercici 5
+// Gestió d'errors amb async/await: Modifica la funció de l'exercici 4 per a que capturi qualsevol possible error utilitzant un bloc try/catch.
+
+
+const promessa2 = (input) =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (input === "Hola"){
+                resolve ("Hola");
+            } else {
+                reject ("Error");
+            }
+        }, 2000);
+    });
+}
+
+async function fn (input) {
+    try {
+        let result = await promessa2 (input);
+        console.log(result);
+    } catch (error){
+        console.log (error);
+    }
+}
+fn ("Hola");
+fn("Adéu")
+
+
+// Nivell 3
+// Exercici 6
+// Promise.all: Crea dues promeses que es resolguin després de 2 i 3 segons, respectivament. Utilitza Promise.all per a esperar que ambdues promeses es resolguin, i imprimeix els resultats a la consola.
+
+const promesa1 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Promesa 1 resolta després de 2 segons");
+    }, 2000);
+});
+
+
+const promesa2 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve("Promesa 2 resolta després de 3 segons");
+    }, 3000);
+});
+
+
+Promise.all([promesa1, promesa2])
+    .then((resultats) => {
+        console.log(resultats);
+    })
+    .catch((error) => {
+        console.error("S'ha produït un error:", error);
+    });
